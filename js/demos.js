f560d55f5aa3f5de3ffb0cdfab3ac53cccc72839
Adrian Capacite <capacite.adrian.thomas@gmail.com>
2023-02-10 12:50:57 +0000

    Fixed message practice demo

--- js/demos.js
+++ js/demos.js
@@ -359,18 +359,18 @@ define([], function () {
   var basicPractice = {
     title: 'Practice with remote',
     key: 'practice-with-remote',
-    message: 'Practice git commands with a remote repository</br>\
-    - \`git clone\` to clone a remote repository (don\'t do this step as</br>\
-      this example is already cloned)</br>\
-    - \`git pull\` to pull changes from the remote repository</br>\
-    - \'git commit\' to commit changes</br>\
-    - \`git push\` to push changes to the remote repository</br>\
-    </br>\
-    Instructions: </br>\
-    1. The remote is ahead by one commit, pull the changes</br>\
-    2. Commit 4 changes with each commit having a different message with</br>\
-       the words: \'Git\', \'Good\', \'With\', \'Adrian\'  </br>\
-    3. Push the changes to the remote repository</br>',
+  message: 'Practice git commands with a remote repository</br>' +
+    '- \`git clone\` to clone a remote repository (don\'t do this step as</br>' +
+    '  this example is already cloned)</br>' +
+    '- \`git pull\` to pull changes from the remote repository</br>' +
+    '- \`git commit\` to commit changes</br>' +
+    '- \`git push\` to push changes to the remote repository</br>' +
+    '</br>' +
+    'Instructions: </br>' +
+    '1. The remote is ahead by one commit, pull the changes</br>' +
+    '2. Commit 4 changes with each commit having a different message with</br>' +
+    '   the words: \'Git\', \'Good\', \'With\', \'Adrian\'  </br>' +
+    '3. Push the changes to the remote repository</br>',
     commitData: [
         {id: 'e137e9b', tags: ['master', 'origin/master'], message: 'first commit'},
     ],
